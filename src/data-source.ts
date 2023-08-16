edeafad122c5fe548662a7fd627e8b5cc5310e28
Franco <francogomez23800@gmail.com>
2023-08-16 20:03:33 -0300

    database ohio

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,7 +4,7 @@ import { Contact } from "./entity/Contact"
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "https://frang-backend-g5v2-dev.fl0.io/",
+    host: "ep-rapid-lake-90709426.us-east-2.aws.neon.tech:5432/frang-database",
     port: 3306,
     username: "root",
     password: "Password123!",
