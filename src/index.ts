2dc8950d73e9214cefe5d2c7fcc1af194df9a622
Franco <francogomez23800@gmail.com>
2023-08-16 20:20:52 -0300

    ssl

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,17 @@ import { AppDataSource } from "./data-source";
 import cors from "cors";
 import helmet from "helmet";
 import routes from "./routes";
+import https from 'https';
+import http from 'http';
+import fs from 'fs';
+
 const PORT = process.env.PORT || 3000;
+
+// Configura las opciones del certificado SSL
+const sslOptions = {
+  key: fs.readFileSync('path/to/private-key.pem'),
+  cert: fs.readFileSync('path/to/certificate.pem')
+};
 
 AppDataSource.initialize()
   .then(async () => {
@@ -19,9 +29,23 @@ AppDataSource.initialize()
     // setup express app here
     // ...
 
-    // start express server
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+    // Crea el servidor HTTPS
+    const server = https.createServer(sslOptions, app);
+
+    // Inicia el servidor en el puerto 443 (puerto HTTPS)
+    server.listen(443, () => {
+      console.log('Servidor HTTPS iniciado en el puerto 443');
+    });
+
+    // Crea un servidor HTTP para redireccionar a HTTPS
+    const httpServer = http.createServer((req, res) => {
+      res.writeHead(301, { 'Location': 'https://' + req.headers.host + req.url });
+      res.end();
+    });
+
+    // Inicia el servidor HTTP en el puerto 80 (puerto HTTP)
+    httpServer.listen(80, () => {
+      console.log('Servidor HTTP de redirección iniciado en el puerto 80');
     });
 
   })
