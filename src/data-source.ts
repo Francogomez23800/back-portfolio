d5278ede1b45b461239413f97e70efe8f5f513d3
Franco <francogomez23800@gmail.com>
2023-08-16 20:05:36 -0300

    debuggin database conection

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,10 +4,10 @@ import { Contact } from "./entity/Contact"
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "ep-rapid-lake-90709426.us-east-2.aws.neon.tech:5432/frang-database",
-    port: 3306,
-    username: "root",
-    password: "Password123!",
+    host: "ep-rapid-lake-90709426.us-east-2.aws.neon.tech",
+    port: 5432,
+    username: "fl0user",
+    password: "Jg6XlqKOP5bz",
     database: "Portfolio",
     synchronize: true,
     logging: false,
