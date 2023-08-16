4114dc81d352754ac94bd756c6f2284f7e9a6a6e
Franco <francogomez23800@gmail.com>
2023-08-16 20:28:34 -0300

    debuggin

--- src/index.ts
+++ src/index.ts
@@ -10,40 +10,37 @@ import fs from 'fs';
 
 const PORT = process.env.PORT || 3000;
 
-// Configura las opciones del certificado SSL
-const sslOptions = {
-  key: fs.readFileSync('path/to/private-key.pem'),
-  cert: fs.readFileSync('path/to/certificate.pem')
-};
-
 AppDataSource.initialize()
   .then(async () => {
-    // create express app
+    // Crear la instancia de la aplicación Express
     const app = express();
     app.use(bodyParser.json());
 
     app.use(cors());
     app.use(helmet());
-    // register express routes from defined application routes
     app.use("/", routes);
-    // setup express app here
-    // ...
 
-    // Crea el servidor HTTPS
-    const server = https.createServer(sslOptions, app);
+    // Configurar opciones para el certificado SSL
+    const sslOptions = {
+      key: fs.readFileSync('path/to/private-key.pem'),
+      cert: fs.readFileSync('path/to/certificate.pem')
+    };
 
-    // Inicia el servidor en el puerto 443 (puerto HTTPS)
-    server.listen(443, () => {
+    // Crear un servidor HTTPS con certificado SSL
+    const httpsServer = https.createServer(sslOptions, app);
+
+    // Escuchar en el puerto 443 para conexiones HTTPS
+    httpsServer.listen(443, () => {
       console.log('Servidor HTTPS iniciado en el puerto 443');
     });
 
-    // Crea un servidor HTTP para redireccionar a HTTPS
+    // Redireccionar las solicitudes HTTP a HTTPS
     const httpServer = http.createServer((req, res) => {
-      res.writeHead(301, { 'Location': 'https://' + req.headers.host + req.url });
+      res.writeHead(301, { 'Location': `https://${req.headers.host}${req.url}` });
       res.end();
     });
 
-    // Inicia el servidor HTTP en el puerto 80 (puerto HTTP)
+    // Escuchar en el puerto 80 para conexiones HTTP (redireccionamiento)
     httpServer.listen(80, () => {
       console.log('Servidor HTTP de redirección iniciado en el puerto 80');
     });
