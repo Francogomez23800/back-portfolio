89281bfe3e907949bf4e6d582297475bc9dd1685
FranG <121100496+Francogomez23800@users.noreply.github.com>
2023-08-16 19:31:44 -0300

    database

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,7 +4,7 @@ import { Contact } from "./entity/Contact"
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "https://frang-backend-g5v2-dev.fl0.io/",
+    host: "frang-backend-g5v2-dev.fl0.io",
     port: 3306,
     username: "root",
     password: "Password123!",
