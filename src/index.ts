4206bbfb0b55459997e9cdf45cbfe2904c0a6cc7
Franco <francogomez23800@gmail.com>
2023-08-16 20:57:29 -0300

    debuggin this pos

--- src/index.ts
+++ src/index.ts
@@ -4,24 +4,50 @@ import { AppDataSource } from "./data-source";
 import cors from "cors";
 import helmet from "helmet";
 import routes from "./routes";
+import https from 'https';
+import http from 'http';
+import fs from 'fs';
+import path from 'path';
+
 const PORT = process.env.PORT || 3000;
 
 AppDataSource.initialize()
   .then(async () => {
-    // create express app
+    // Crear la instancia de la aplicación Express
     const app = express();
     app.use(bodyParser.json());
 
     app.use(cors());
     app.use(helmet());
-    // register express routes from defined application routes
     app.use("/", routes);
-    // setup express app here
-    // ...
 
-    // start express server
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+    // Obtener las rutas absolutas a los archivos de clave privada y certificado
+    const privateKeyPath = path.join(__dirname, 'path', 'to', 'private-key.pem');
+    const certificatePath = path.join(__dirname, 'path', 'to', 'certificate.pem');
+
+    // Configurar opciones para el certificado SSL
+    const sslOptions = {
+      key: fs.readFileSync(privateKeyPath),
+      cert: fs.readFileSync(certificatePath)
+    };
+
+    // Crear un servidor HTTPS con certificado SSL
+    const httpsServer = https.createServer(sslOptions, app);
+
+    // Escuchar en el puerto 443 para conexiones HTTPS
+    httpsServer.listen(443, () => {
+      console.log('Servidor HTTPS iniciado en el puerto 443');
+    });
+
+    // Redireccionar las solicitudes HTTP a HTTPS
+    const httpServer = http.createServer((req, res) => {
+      res.writeHead(301, { 'Location': `https://${req.headers.host}${req.url}` });
+      res.end();
+    });
+
+    // Escuchar en el puerto 80 para conexiones HTTP (redireccionamiento)
+    httpServer.listen(80, () => {
+      console.log('Servidor HTTP de redirección iniciado en el puerto 80');
     });
 
   })
