ee8bfd2e2286b9e04fa3df83ffbb755ac99c6af2
Franco <francogomez23800@gmail.com>
2023-08-16 21:16:45 -0300

    adding name

--- src/data-source.ts
+++ src/data-source.ts
@@ -8,7 +8,7 @@ export const AppDataSource = new DataSource({
     port: 5432,
     username: "fl0user",
     password: "Jg6XlqKOP5bz",
-    database: "frang-database",
+    database: "frang-databases",
     synchronize: true,
     logging: false,
     entities: [Contact],
